a90866bcd040b69d3396e485bd478f6e5430bf99
Alexandra Bonta <alexandra.bonta12@thehutgroup.com>
2023-03-07 10:50:09 +0000

    meeting, book, user models added

--- src/main/java/com/example/bookclub/Model/Book.java
+++ src/main/java/com/example/bookclub/Model/Book.java
@@ -1,4 +1,43 @@
 package com.example.bookclub.Model;
 
 public class Book {
+    private int BOOK_ID;
+    public String title;
+    public String[] author;
+    public String[] description;
+
+    public Book(){}
+    public int getBOOK_ID() {
+        return BOOK_ID;
+    }
+
+    public void setBOOK_ID(int BOOK_ID) {
+        this.BOOK_ID = BOOK_ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String[] getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String[] author) {
+        this.author = author;
+    }
+
+    public String[] getDescription() {
+        return description;
+    }
+
+    public void setDescription(String[] description) {
+        this.description = description;
+    }
+
+
 }
--- src/main/java/com/example/bookclub/Model/User.java
+++ src/main/java/com/example/bookclub/Model/User.java
@@ -1,4 +1,35 @@
 package com.example.bookclub.Model;
 
 public class User {
+    private int USER_ID;
+    public String[] userName;
+    public String userEmail;
+
+    public User(){}
+    public int getUSER_ID() {
+        return USER_ID;
+    }
+
+    public void setUSER_ID(int USER_ID) {
+        this.USER_ID = USER_ID;
+    }
+
+    public String[] getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String[] userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+
+
 }
